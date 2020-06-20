00b63e0dd4e80cb42bd298da0a665cdb99b61698
Afshin Teymoori <>
2020-06-20 19:21:35 +1000

    add onChange method to change the state on input text change event

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -2,9 +2,27 @@ import React from "react";
 class CoursesPages extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { title: "" };
+    this.onChange = this.onChange.bind(this);
   }
+
+  //   onChange = (event) => {
+  //     this.setState({ title: event.target.value });
+  //   };
+
+  onChange = function (event) {
+    this.setState({ title: event.target.value });
+  };
   render() {
-    return <span>{this.props.name}</span>;
+    return (
+      <div>
+        <span>
+          {this.props.name}:{this.state.title}
+        </span>
+        <br />
+        <input value={this.state.title} onChange={this.onChange}></input>
+      </div>
+    );
   }
 }
 export default CoursesPages;
