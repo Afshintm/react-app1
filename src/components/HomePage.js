1bfd4b2a2db847881d0a573dc677a089b2902b4c
Afshin Teymoori <>
2020-06-20 11:54:55 +1000

    add simple menu

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,14 @@
-import React from "React";
+import React from "react";
+//React components are instantiated. So making the function start with capital resemble the classes in JS which are functions with the constructor starting with capital letter
 function HomePage() {
-  return <div className="jumborton"> Hello World .</div>;
+  //returning JSX
+  return (
+    <div className="jumbotron">
+      <h1>Hello World</h1>
+      <p>This is the homepage of the react app.</p>
+      <a href="/about">About</a>
+    </div>
+  );
 }
+// default because there is just one component
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+//creating a const that value is reference to react-dom
+import { render } from "react-dom";
+//import HomePage from "./components/HomePage.js";
+//import AboutPage from "./components/AboutPage.js";
+import App from "./components/App.js";
+
+render(<App />, document.getElementById("root"));
